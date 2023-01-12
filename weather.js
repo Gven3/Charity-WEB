13df3d3be67feda8a511291a1227edb5b6e031bc
Gven3 <giogvenetadze13@gmail.com>
2023-01-12 14:56:57 +0400

    :)

--- weather.js
+++ weather.js
@@ -1,3 +1,5 @@
+// Weather API
+
 const dataCity = document.querySelector("#dataCity");
 const dataDate = document.querySelector("#dataDate");
 const dataTemp = document.querySelector("#dataTemp");
@@ -19,3 +21,35 @@ fetch(link)
     dataHumidity.innerText = data.main.humidity;
     dataWind.innerText = data.wind.speed;
   });
+
+// Add Country JS
+
+const addButton = document.getElementById("addButt");
+const input = document.getElementById("input");
+const icon = document.querySelector("#iconDiv");
+const form = document.querySelector(".addDiv");
+
+addButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  input.classList.toggle("active");
+  icon.classList.toggle("active");
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const link =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    input.value +
+    "&units=metric&appid=d6e73b4282d3d3a1afc8a0b11b906f28";
+  fetch(link)
+    .then((resp) => resp.json())
+    .then((data) => {
+      console.log(data);
+      dataCity.innerText = data.name;
+      dataTemp.innerText = data.main.temp;
+      dataVisibility.innerHTML = data.visibility;
+      dataFeelsLike.innerText = data.main.feels_like;
+      dataHumidity.innerText = data.main.humidity;
+      dataWind.innerText = data.wind.speed;
+    });
+});
